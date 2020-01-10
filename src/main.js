cefdad0f2cbd6b96b7995bdbe50efc857aff81f7
liulijun <1937249556@qq.com>
2020-01-10 18:02:52 +0800

    “
    提交：bookmark360.html

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 Vue.config.productionTip = false  //消息提示的环境配置，设置为开发环境或者生产环境
 
 new Vue({
-  router,
+  router,       //挂载将路由器到根元素'#app'下，挂载后就可以在'#app'对应的元素下通过<router-link>标签使用了。
   store,
-  render: h => h(App)
+  render: h => h(App)   //使用render函数渲染App组件来创建HTML，类似于template选项功能。
 }).$mount('#app')
