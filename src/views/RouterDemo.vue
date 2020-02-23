ea3bf990bb3823a7c02a6b6f8bb55ad109a4a07a
liulijun <1937249556@qq.com>
2020-02-23 21:37:44 +0800

    提交：RouterDemo.vue

--- src/views/RouterDemo.vue
+++ src/views/RouterDemo.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h1># 路由器案例</h1>
+    <p>当前路由：{{username}}</p>
+    <div>
+        <router-link to="/home/zhangSan">Home</router-link> |
+        <router-link to="/about/liShi">About</router-link>        
+    </div>
+    <router-view/>  <!-- 路由出口。路由匹配到的组件将渲染在这里 -->
     
+
   </div>
   
 </template>
 
-<script type="text/javascript">
+<script type="text/javascript">    
 
 export default{
     name:'example',
@@ -18,7 +25,13 @@ export default{
         
       }; 
     },
-    computed:{},
+    computed:{
+        username:function(){
+            console.log(`当前路由：${JSON.stringify(this.$route.params)}`);
+            console.log(`当前路由Name：${JSON.stringify(this.$route.name)}`);
+            return this.$route.params.username;
+        },
+    },
     methods:{},
     watch:{},
     components:{},
--- src/vueRouter.js
+++ src/vueRouter.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import router from './router'
+import store from './store'
 import RouterDemo from '@/views/RouterDemo.vue';
 
 
 new Vue({
+    router,     //将Vue路由器 router注入所有子组件。
+    store,      //将统一管理状态store注入所有子组件
     render: h => h(RouterDemo,{
       props:{   //赋值组件定义的props
 
